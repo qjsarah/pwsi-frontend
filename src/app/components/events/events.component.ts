c1fe45a8b8a62fcff7a375a22ada950ba784c643
qjsarah <165972652+qjsarah@users.noreply.github.com>
2025-03-27 00:11:10 +0800

    update

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -19,7 +19,7 @@ export class EventsComponent {
   post_header = "Making an Impact Our Initiatives";
   post_para = "Showcasing programs, events, and success stories that empower and celebrate the PWD community, while keeping you updated on their latest achievements and milestones.";
 
-  readonly APIUrl = 'http://localhost:5038/posts/';
+  readonly APIUrl = 'https://pwsi-backend.onrender.com/posts/';
   constructor(private http:HttpClient){}
   title = 'ui';
   posts: any=[];
